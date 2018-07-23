fe5e600b937f052c4985eaf4bc9e03fe3b6c2c58
github username <github email address>
2018-07-22 21:15:38 -0400

    Initial commit

---
+++ nike.rb
@@ -0,0 +1,36 @@
+require 'HTTParty'
+require 'Nokogiri'
+require 'byebug'
+
+class Scraper
+
+attr_accessor :parse_page
+
+  def initialize
+    doc = HTTParty.get("https://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3")
+    @parse_page ||= Nokogiri::HTML(doc) #memorized the @parse_page so it only gets assigned once
+  end
+
+  def get_names
+  item_container.css(".product-name").css("p").children.map { |name| name.text }.compact
+end
+
+  def get_prices
+  item_container.css(".product-price").css("span.local").children.map { |price| price.text}.compact
+  end
+
+  private
+
+  def item_container
+    parse_page.css(".grid-item-info")
+  end
+
+  scraper = Scraper.new
+  names = scraper.get_names
+  prices = scraper.get_prices
+
+  (0...prices.size).each do |index| #three dots don't include the last digit. Will behave like 0...image_links -1
+    puts "- - - index: #{index + 1} - - -"
+    puts "Name: #{names[index]} | price: #{prices[index]}"
+  end
+end
