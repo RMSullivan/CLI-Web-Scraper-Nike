676ec65b27d0e09a03719b64cab577ec0dbf0335
github username <github email address>
2019-06-13 08:24:59 -0400

    Update nike.rb
    updated links and gems

--- nike.rb
+++ nike.rb
@@ -7,7 +7,7 @@ class Scraper
 attr_accessor :parse_page
 
   def initialize
-    doc = HTTParty.get("https://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3")
+    doc = HTTParty.get("https://store.nike.com/us/en_us/pw/womens-lifestyle-shoes/7ptZoneZoi3")
     @parse_page ||= Nokogiri::HTML(doc) #memorized the @parse_page so it only gets assigned once
   end
 
